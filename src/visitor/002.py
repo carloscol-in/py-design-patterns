ec1fb6dbfb9873dcc5414b41f2788534ed9715e7
Carlos C <contact@carloscolin.me>
2022-09-18 01:42:50 -0500

    Classic visitor implementation

---
+++ src/visitor/002.py
@@ -0,0 +1,86 @@
+
+from abc import ABC
+from re import S
+
+
+def _qualname(obj):
+    """Get the fully-qualified name of an object (including module)."""
+    return obj.__module__ + '.' + obj.__qualname__
+
+
+def _declaring_class(obj):
+    """Get the name of the class that declared an object."""
+    name = _qualname(obj)
+    return name[:name.rfind('.')]
+
+
+# Stores the actual visitor methods
+_methods = {}
+
+
+# Delegating visitor implementation
+def _visitor_impl(self, arg):
+    """Actual visitor method implementation."""
+    method = _methods[(_qualname(type(self)), type(arg))]
+    return method(self, arg)
+
+def visitor(arg_type):
+    def decorator(fn):
+        declaring_class = _declaring_class(fn)
+        _methods[(declaring_class, arg_type)] = fn
+
+        return _visitor_impl
+
+    return decorator
+
+class Expression(ABC):
+    def print(self, b):
+        return ExpressionPrinter.print(self, b)
+
+class DoubleExpression(Expression):
+    def __init__(self, value):
+        self.value = value
+        
+    def accept(self, visitor):
+        visitor.visit(self)
+
+class AdditionExpression(Expression):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+        
+    def accept(self, visitor):
+        visitor.visit(self)
+
+class ExpressionPrinter:
+    def __init__(self):
+        self.buffer = []
+
+    @visitor(DoubleExpression)
+    def visit(self, de):
+        self.buffer.append(str(de.value))
+
+    @visitor(AdditionExpression)
+    def visit(self, ae):
+        self.buffer.append('(')
+        ae.left.accept(self)
+        self.buffer.append('+')
+        ae.right.accept(self)
+        self.buffer.append(')')
+
+    def __str__(self):
+        return ''.join(self.buffer)
+
+
+if __name__ == '__main__':
+    e = AdditionExpression(
+        DoubleExpression(1),
+        AdditionExpression(
+            DoubleExpression(2),
+            DoubleExpression(3)
+        )
+    )
+
+    printer = ExpressionPrinter()
+    printer.visit(e)
+    print(printer)
